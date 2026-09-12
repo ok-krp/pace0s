a9d8895ab1d2caf3dd3ed4bb12e51125a4d54895
ok-krp <chillz.120fps@gmail.com>
2026-09-12 19:59:56 +0200

    fix(ui): center dialogs in viewport

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -39,7 +39,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed !inset-0 !m-auto z-50 grid h-fit w-[calc(100vw-2rem)] max-w-lg max-h-[85vh] overflow-y-auto !translate-x-0 !translate-y-0 gap-4 glass-card p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
+        "fixed left-1/2 top-1/2 z-50 grid h-fit w-[calc(100vw-2rem)] max-w-lg max-h-[85vh] -translate-x-1/2 -translate-y-1/2 overflow-y-auto gap-4 glass-card p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
         className,
       )}
       {...props}
