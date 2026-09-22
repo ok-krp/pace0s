673667339bcc3466db21dfe1a7f355a96f294764
ok-krp <chillz.120fps@gmail.com>
2026-09-22 19:37:40 +0200

    chore(e2ee): load explicit migration trigger

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import "./lib/health.migration";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
