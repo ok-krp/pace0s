f408610b7e2f35ab99ea8cc610a166bb8d499fbf
ok-krp <chillz.120fps@gmail.com>
2026-09-25 11:48:40 +0200

    test(android): add Health queue instrumentation dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     kotlinOptions { jvmTarget = "17" }
 }
 
-dependencies {
+dependencies {\n    testImplementation("junit:junit:4.13.2")\n    androidTestImplementation("androidx.test.ext:junit:1.2.1")\n    androidTestImplementation("androidx.test:runner:1.6.2")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.health.connect:connect-client:1.1.0")
